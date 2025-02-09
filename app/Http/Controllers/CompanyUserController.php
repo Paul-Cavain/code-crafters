d97ac5ca3c61cdb66f9d6828a5dc799becc661b5
petson <83289626+nathanaeli@users.noreply.github.com>
2025-02-09 16:28:56 +0300

    update
    update

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -7,17 +7,17 @@ use Illuminate\Http\Request;
 
 class CompanyUserController extends Controller
 {
-    // register route function
+  
     public function register() {
         return view('register');
     }
 
-    // login route function
+  
     public function login() {
         return view('login');
     }
 
-    // approval route function
+   
     public function approval() {
         return view('approval');
     }
